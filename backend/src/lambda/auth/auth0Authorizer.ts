68c41f326d8f8887eddf4d308d71cc25b44c8999
Mustafa Ryad <mustafa.ryad@emushrif.om>
2023-03-03 10:13:31 +0200

    Solve missing dashes in cert variable

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -9,9 +9,6 @@ import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
 const jwksUrl =
   'https://dev-muqkwpoft1cle3ds.us.auth0.com/.well-known/jwks.json'
 
@@ -61,10 +58,6 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-
   const response = await Axios.get(jwksUrl)
   const keys = response.data.keys
 
@@ -78,7 +71,8 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
 
   const pemData = signingKeys.x5c[0]
 
-  const cert = `-----BEGIN CERTIFICATE----\n${pemData}\n-----END CERTIFICATE-----`
+  const cert = `------BEGIN CERTIFICATE-----\n${pemData}\n-----END CERTIFICATE-----`
+
   const verifiedToken = verify(token, cert, {
     algorithms: ['RS256']
   }) as JwtPayload
